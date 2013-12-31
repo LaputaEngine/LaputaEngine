36604a7db90fb93a0ed2bfe2010a0a6bab2b2ec6
ffred <ffred@sfu.ca>
2013-12-30 21:19:44 -0800

    Added tests for vector arithmetic operations.

--- LaputaEngine/LaputaTests/Laputa3D/LptaVector.cpp
+++ LaputaEngine/LaputaTests/Laputa3D/LptaVector.cpp
@@ -19,14 +19,12 @@ private:
 	bool oldSSECapable;
 };
 
-TEST(LptaVectorTest, AddToNormalCase)
+TEST(LptaVectorTest, VectorGettersNormalCase)
 {
-	LptaVector v0(0.0f, 0.0f, 0.0f);
-	LptaVector v1(1.0f, 2.0f, 3.0f);
-	v0 += v1;
-	ASSERT_EQ(1.0f, v0.GetX());
-	ASSERT_EQ(2.0f, v0.GetY());
-	ASSERT_EQ(3.0f, v0.GetZ());
+	LptaVector v(0.0f, 1.0f, 2.0f);
+	ASSERT_EQ(v.GetX(), 0.0f);
+	ASSERT_EQ(v.GetY(), 1.0f);
+	ASSERT_EQ(v.GetZ(), 2.0f);
 }
 
 TEST(LptaVectorTest, LengthNormalCase)
@@ -39,4 +37,81 @@ TEST_F(LptaVectorNoSSETest, LengthNormalCase)
 {
 	::LptaVector v(1.0f, 2.0f, 2.0f);
 	ASSERT_EQ(3.0f, v.Length());
+}
+
+TEST(LptaVectorTest, AddToNormalCase)
+{
+	LptaVector v0(0.0f, 0.0f, 0.0f);
+	LptaVector v1(1.0f, 2.0f, 3.0f);
+	v0 += v1;
+	ASSERT_EQ(1.0f, v0.GetX());
+	ASSERT_EQ(2.0f, v0.GetY());
+	ASSERT_EQ(3.0f, v0.GetZ());
+}
+
+TEST(LptaVectorTest, SubtractFromNormalCase)
+{
+	LptaVector v0(2.0f, 3.0f, 4.0f);
+	LptaVector v1(1.0f, 1.0f, 1.0f);
+	v0 -= v1;
+	ASSERT_EQ(1.0f, v0.GetX());
+	ASSERT_EQ(2.0f, v0.GetY());
+	ASSERT_EQ(3.0f, v0.GetZ());
+}
+
+TEST(LptaVectorTest, MultipliedByNormalCase)
+{
+	LptaVector v0(1.0f, 2.0f, 3.0f);
+	LptaVector v1(2.0f, 2.0f, 2.0f);
+	v0 *= v1;
+	ASSERT_EQ(2.0f, v0.GetX());
+	ASSERT_EQ(4.0f, v0.GetY());
+	ASSERT_EQ(6.0f, v0.GetZ());
+}
+
+TEST(LptaVectorTest, DividedByNormalCase)
+{
+	LptaVector v0(4.0f, 6.0f, 8.0f);
+	LptaVector v1(2.0f, 2.0f, 2.0f);
+	v0 /= v1;
+	ASSERT_EQ(2.0f, v0.GetX());
+	ASSERT_EQ(3.0f, v0.GetY());
+	ASSERT_EQ(4.0f, v0.GetZ());
+}
+
+TEST(LptaVectorTest, MultiplyByVectorNormalCase)
+{
+	LptaVector v0(1.0f, 2.0f, 3.0f);
+	LptaVector v1(4.0f, 5.0f, 6.0f);
+	float f = v0 * v1;
+	ASSERT_EQ(f, 32.0f);
+}
+
+TEST(LptaVectorTest, MultiplyByScalarNormalCase)
+{
+	LptaVector v0(1.0f, 2.0f, 3.0f);
+	LptaVector v1 = v0 * 2.0f;
+	ASSERT_EQ(2.0f, v1.GetX());
+	ASSERT_EQ(4.0f, v1.GetY());
+	ASSERT_EQ(6.0f, v1.GetZ());
+}
+
+TEST(LptaVectorTest, AddVectorsNormalCase)
+{
+	LptaVector v0(4.0f, 6.0f, 8.0f);
+	LptaVector v1(2.0f, 2.0f, 2.0f);
+	LptaVector v2 = v0 + v1;
+	ASSERT_EQ(6.0f, v2.GetX());
+	ASSERT_EQ(8.0f, v2.GetY());
+	ASSERT_EQ(10.0f, v2.GetZ());
+}
+
+TEST(LptaVectorTest, SubtractVectorsNormalCase)
+{
+	LptaVector v0(4.0f, 6.0f, 8.0f);
+	LptaVector v1(2.0f, 2.0f, 2.0f);
+	LptaVector v2 = v0 - v1;
+	ASSERT_EQ(2.0f, v2.GetX());
+	ASSERT_EQ(4.0f, v2.GetY());
+	ASSERT_EQ(6.0f, v2.GetZ());
 }
